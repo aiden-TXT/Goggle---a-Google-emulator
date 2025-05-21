5677b907e7b56391247b09220a9fc105511523f7
Aiden <zachriaz@hotmail.com>
2025-05-21 17:35:07 +0100

    Create search-engine.h
    hwudgwduywgdUI

---
+++ search-engine.h
@@ -0,0 +1,12 @@
+#ifndef SEARCH_ENGINE_H
+#define SEARCH_ENGINE_H
+
+#include <string>
+#include <vector>
+
+class SearchEngine {
+public:
+    void search(const std::string& query);
+};
+
+#endif // SEARCH_ENGINE_H
