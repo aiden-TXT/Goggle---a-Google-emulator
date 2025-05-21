e156e8e8d2db04cff4b3a3c755792416ba195b3b
Aiden <zachriaz@hotmail.com>
2025-05-21 17:38:55 +0100

    Create utils.h

---
+++ utils.h
@@ -0,0 +1,12 @@
+#ifndef UTILS_H
+#define UTILS_H
+
+#include <string>
+
+namespace Utils {
+    std::string toLowerCase(const std::string& str);
+    void clearScreen();
+    void pause();
+}
+
+#endif // UTILS_H
