53e9e88680442932fce8fe866a3141bab63c756b
Aiden <zachriaz@hotmail.com>
2025-05-21 17:35:47 +0100

    Create execution-manager.h

---
+++ execution-manager.h
@@ -0,0 +1,11 @@
+#ifndef EXECUTION_MANAGER_H
+#define EXECUTION_MANAGER_H
+
+#include <string>
+
+class ExecutionManager {
+public:
+    void runProgram(const std::string& programPath);
+};
+
+#endif // EXECUTION_MANAGER_H
